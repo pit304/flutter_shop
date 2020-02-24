bb5351b046dec2084a8f1a80fba6296b25e77c16
Alexandru Petrencu <alex.petrencu@gmail.com>
2020-02-24 08:49:04 +0100

    lesson 195: added fonts and providers

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    final Product loadedProduct = Provider.of<Product>(context);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: loadedProduct.id,
+            );
+          },
+          child: Image.network(
+            loadedProduct.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(loadedProduct.isFavorite
+                ? Icons.favorite
+                : Icons.favorite_border),
+            onPressed: () {
+              loadedProduct.toggleFavoriteStatus();
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            loadedProduct.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
